4813fa425133ead19357d4f6cfcbe659d902822a
Permagate <Kend654@gmail.com>
2015-02-18 12:19:55 +0700

    [04 - Practice] Refactor page title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(title = "")
+    final_title = "Ruby on Rails Tutorial Sample App"
+    if title.empty?
+      final_title
+    else
+      "#{title} | #{final_title}"
+    end
+  end
+
 end
