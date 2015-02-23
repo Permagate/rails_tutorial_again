ae808a3dbddfd1e7109f0ad933eeb6f8e76c5b69
Permagate <Kend654@gmail.com>
2015-02-23 08:36:47 +0700

    [05 - Practice] Add another integration test.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,13 +4,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "layout links" do
+
+  def setup
+    @common_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "home links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path  
+  end
+
+  test "signup title" do
+    get signup_path
+    assert_template "users/new"
+    assert_select "title", "Sign up | #{@common_title}"
   end
   
 end
