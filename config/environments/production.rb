b6b884ea2e0827570f8afc7551d96d703a9aa808
Permagate <Kend654@gmail.com>
2015-02-25 09:29:38 +0700

    [10 - Theory] Add working email config in prod env.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'young-sea-2407.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtpcorp.com',
+    :port           => '2525',
+    :authentication => :plain,
+    :user_name      => ENV['SMTP2GO_USERNAME'],
+    :password       => ENV['SMTP2GO_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
