228eb3d7c0d55722c27248d77f364cd6a3f7eeca
Permagate <Kend654@gmail.com>
2015-02-25 08:32:08 +0700

    [10 - Theory] Complete activation.

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,7 +2,7 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
-  def account_activation
+  def account_activation(user, &block)
     user = User.first
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
